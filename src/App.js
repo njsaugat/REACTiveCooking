9f9ac639803fb6d13855a829a299c1d078ae617d
njsaugat <njsaugat@gmail.com>
2022-04-04 11:58:08 +0545

    changes made to render other components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
-import React,{createContext, useState} from 'react';
-
+import React,{useState} from 'react';
+import MenuAdder from './Components/MenuAdder';
 
 function App() {
-  const sdf='sdfjkl;sdf;lsdfk;sdf';
- return null; 
- 
+  const [clicked,setClick]=useState(false);
+  // const clicked=false;
+ return(
+   <>
+   <div className='contain d-flex flex-row'>
+        <div className='left w-50 d-flex justify-content-center h-50'>
+        <button type="button" className="btn btn-primary my-1  " onClick={()=>setClick(true)}> Add recipe</button>
+        </div>
+        <div className='right w-50'>
+            <MenuAdder click={clicked}/>
+        </div>
+   </div>
+   </>
+ )
 }
 export default App;
